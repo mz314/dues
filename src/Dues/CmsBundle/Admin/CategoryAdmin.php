446f17112c7b8fbe74863eefcf5998f4ed82bfe4
Maciej Zakrzewski <maciej.zakrzewski@polcode.net>
2014-07-24 15:30:54 +0200

    cms admin

---
+++ src/Dues/CmsBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dues\CmsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends Admin {
+
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper) {
+        $formMapper
+            ->add('username', 'text', array('label' => 'User name'))
+                ->add('display_name', 'text', array('label' => 'Display name'))
+        
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+//        $datagridMapper
+//            ->add('title')
+//            ->add('author')
+//        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper
+                ->addIdentifier('id')
+                ->addIdentifier('username')
+                ->addIdentifier('display_name')
+
+        ;
+    }
+
+}
